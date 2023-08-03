461e5de8288ad37d9de96f0a55a5d6acbf5c6751
HanDongHun <95519378+mrsuit0114@users.noreply.github.com>
2023-08-03 19:50:17 +0900

    spa, router

---
+++ Study/BlazorStart/BlazorApp/BlazorApp/Data/FoodService.cs
@@ -0,0 +1,38 @@
+﻿namespace BlazorApp.Data
+{
+    public class Food
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+    
+    public interface IFoodService
+    {
+        IEnumerable<Food> GetFoods();
+    }
+
+    public class FoodService : IFoodService
+    {
+        public IEnumerable<Food> GetFoods()
+        {
+            List<Food> foods = new List<Food>()
+            {
+                new Food() {Name = "Bibimbap", Price = 7000},
+                new Food() {Name = "Gimbap", Price = 3000},
+                new Food() {Name = "Jjajanbap", Price = 8000}
+            };
+
+            return foods;
+        }
+    }
+
+    public class PaymentService
+    {
+        IFoodService _service;
+        public PaymentService(IFoodService service)
+        {
+            _service = service;
+        }
+    }
+
+}
--- Study/BlazorStart/BlazorApp/BlazorApp/Program.cs
+++ Study/BlazorStart/BlazorApp/BlazorApp/Program.cs
@@ -9,6 +9,16 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
+//Dependency Injection
+builder.Services.AddSingleton<IFoodService, FoodService>();
+// 생성자에서 알아서 연결해준다..?
+builder.Services.AddSingleton<PaymentService>();
+
+// 3가지 모드
+// Singleton  -  서버를 껐다 키지 않는이상 고정
+// Transient  -  요청마다 변함
+// Scoped  -  둘의 중간, 새로고침은 변경인데, 다른 탭을 이용하는 과정에서는 안변함
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
