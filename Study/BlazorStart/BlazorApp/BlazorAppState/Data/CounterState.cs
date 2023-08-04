6998497a202207d19fba2f7fee9a92c65153e7a7
HanDongHun <95519378+mrsuit0114@users.noreply.github.com>
2023-08-04 17:56:26 +0900

    state관리

---
+++ Study/BlazorStart/BlazorApp/BlazorAppState/Data/CounterState.cs
@@ -0,0 +1,29 @@
+﻿namespace BlazorAppState.Data
+{
+    public class CounterState
+    {
+        //public int Count { get;set; }
+        int _count = 0;
+
+        public Action OnStateChanged;
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+            set
+            {
+                _count = value;
+                Refresh();
+            }
+        }
+
+        void Refresh()
+        {
+            OnStateChanged?.Invoke();
+        }
+
+    }
+}
