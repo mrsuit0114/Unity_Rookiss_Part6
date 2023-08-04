82b28069059feb0e5bd22c9f33c62f20c3eb8df1
HanDongHun <95519378+mrsuit0114@users.noreply.github.com>
2023-08-04 17:32:05 +0900

    Form, Validation

--- Study/BlazorStart/BlazorApp/BlazorStudy/Data/WeatherForecast.cs
+++ Study/BlazorStart/BlazorApp/BlazorStudy/Data/WeatherForecast.cs
@@ -1,13 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorStudy.Data
 {
 	public class WeatherForecast
 	{
 		public DateOnly Date { get; set; }
 
+		[Required(ErrorMessage = "Need TempC!")]
+		[Range(typeof(int), "-100","100")]
 		public int TemperatureC { get; set; }
 
 		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+		[Required(ErrorMessage = "Need Sum!")]
+		[StringLength(10,MinimumLength = 0, ErrorMessage = "3~10")]
 		public string? Summary { get; set; }
 	}
 }
