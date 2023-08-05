9251f58739a7947ebcc8a2814a22509a4c41b3e8
HanDongHun <95519378+mrsuit0114@users.noreply.github.com>
2023-08-05 19:08:23 +0900

    Blazor RankingApp #3
    Create

--- Study/BlazorStart/BlazorRankingProject/BlazorRankingProject/Data/Services/RankingService.cs
+++ Study/BlazorStart/BlazorRankingProject/BlazorRankingProject/Data/Services/RankingService.cs
@@ -12,11 +12,25 @@ namespace BlazorRankingProject.Data.Services
 			_context = context;
 		}
 
+		//Create
+		public Task<GameResult> AddGameResult(GameResult gameResult)
+		{
+			_context.GameResults.Add(gameResult);
+			_context.SaveChanges();  //db 실제 저장
+
+			return Task.FromResult(gameResult);  // 용도가 있나 리턴해도 안쓰던데 Read에서는
+		}
+
+		// Read
 		public Task<List<GameResult>> GetGameResultAsync()
 		{
 			List<GameResult> result = _context.GameResults.OrderByDescending(item=> item.Score).ToList();
 			return Task.FromResult(result);
 		}
+
+		//Update
+
+		//Delete
 
 	}
 }
